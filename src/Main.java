4df2013f6ad40a0b627da8810bab48073b49dbd4
IcantthinkOfaGitHuhusername <128sgauthaman@frhsd.com>
2026-09-15 13:36:26 -0400

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,28 @@
 import java.util.*;
 
+
+
 public class Main {
     public static void main(String[] args) {
+        String pw="";
+        int pw_l=8;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("how long do you want your pw, must be minimum of 8");
+        pw_l = sc.nextInt();
+        if(pw_l<8){
+            pw_l=8;
+        }
+        System.out.println("do you want your pw of have an upper case letter, 1 for yes 2 for no");
+        int aw=sc.nextInt();
+
+        if(aw==1){
+        }
+         System.out.println("do you want your pw of have a special character 1 for yes 2 for no");
+        aw=sc.nextInt();
+        if(aw==1){}
+
+
+
 
     }
 }
